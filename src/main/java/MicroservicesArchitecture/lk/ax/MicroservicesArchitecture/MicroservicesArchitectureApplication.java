6af67bb473112be04b6b18cf306eaaedeabbdc5b
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-09-08 07:36:39 +0530

    pr-init

---
+++ src/main/java/MicroservicesArchitecture/lk/ax/MicroservicesArchitecture/MicroservicesArchitectureApplication.java
@@ -0,0 +1,13 @@
+package MicroservicesArchitecture.lk.ax.MicroservicesArchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MicroservicesArchitecture/lk/ax/MicroservicesArchitecture/MicroservicesArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package MicroservicesArchitecture.lk.ax.MicroservicesArchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
